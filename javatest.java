88394674a712d1ce83572c258cf1bcc169219c3f
Linhao Li <jerrylilinhao@gmail.com>
2017-07-29 16:46:26 +1000

    test

---
+++ javatest.java
@@ -0,0 +1,11 @@
+public class javatest{
+  public static void increment ( int x) {
+    x ++;
+  }
+
+  public static void main ( String [] args ) {
+    int p;
+    increment (p);
+    System .out . println (p);
+  }
+}
