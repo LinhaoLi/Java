3ec14e3ddf5aaad7cdef0c18d373db2d801f5d61
Linhao Li <jerrylilinhao@gmail.com>
2017-07-31 14:30:54 +1000

    Hello Java Challenges

--- javatest.java
+++ javatest.java
@@ -1,11 +1,9 @@
 public class javatest{
-  public static void increment ( int x) {
-    x ++;
-  }
-//new update
-  public static void main ( String [] args ) {
-    int p;
-    increment (p);
-    System .out . println (p);
-  }
+  public static void main(String[] asg){
+    String msg = "? siht daer uoy nac ";
+   StringBuilder sb = new StringBuilder ( msg );
+   System .out . println (sb. toString ());
+   sb. reverse ();
+   System .out . println (sb. toString ());
+}
 }
