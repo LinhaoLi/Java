874f75d7005a733d210e5a39b7e96b7b60fac0bd
Linhao Li <jerrylilinhao@gmail.com>
2017-08-05 16:08:24 +1000

    Lecture notes

---
+++ Lab1/ex1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ex1{
+    public static void main(String[] args){
+    Scanner keyboard = new Scanner(System.in);
+    int radius;
+    radius = keyboard.nextInt();
+    while () {
+        if (i == 0||i ==4){
+            System.out.println("+--------+");
+        }
+        else{
+            System.out.println("|        |");
+        }
+        i++;
+    }
+    }
+    keyboard.close();
+}
